5103bb49d700517b99f76423c692764c3f0c75cb
renergynusantara <digitalmarketing.renus@gmail.com>
2023-12-07 13:23:27 +0700

    add redirect metode 301

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
     session_start();
 
     if ( isset($_SESSION["login"]) ) {
-        header("Location: dashboard.php");
+        header("Location: dashboard.php", true, 301);
         exit;
     } elseif(isset($_COOKIE['login'])) {
-        header("Location: dashboard.php");
+        header("Location: dashboard.php", true, 301);
         exit;
     } else {
-        header("Location: login.php");
+        header("Location: login.php", true, 301);
     }
 ?>
