22d8949e4a29322230b0e16bbfa819a0e6347b34
renergynusantara <digitalmarketing.renus@gmail.com>
2023-12-07 17:54:59 +0700

    remove redirect functions

--- index.php
+++ index.php
@@ -4,16 +4,13 @@
     session_start();
 
     if ( isset($_SESSION["login"]) ) {
-        // header("Location: dashboard");
-        // exit();
-        customRedirect("dashboard");
+        header("Location: dashboard");
+        exit();
     } elseif(isset($_COOKIE['login'])) {
-        // header("Location: dashboard");
-        // exit();
-        customRedirect("dashboard");
+        header("Location: dashboard");
+        exit();
     } else {
-        // header("Location: login");
-        // exit();
-        customRedirect("login");
+        header("Location: login");
+        exit();
     }
 ?>
